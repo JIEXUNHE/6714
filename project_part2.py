bc6ba3a30ef4664e8e6b1340432d798bb14cbea4
Matchman <33885018+JIEXUNHE@users.noreply.github.com>
2020-09-09 12:25:21 +0800

    Add files via upload

---
+++ project_part2.py
@@ -0,0 +1,6 @@
+import numpy as np
+import xgboost as xgb
+
+def disambiguate_mentions(train_mentions, train_labels, dev_mentions, men_docs, parsed_entity_pages):
+    pass ## You can replace this line with your code...
+
